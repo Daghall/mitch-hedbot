1bf183fff5c61639fb2cf5082f1139141dc554a4
Markus Daghall <markus@daghall.se>
2022-11-05 14:27:47 +0100

    Add dev parameter to fire dev web hook
    In order to test the web hook in another channel, without having to
    change the web hook URL all the time. The test function in AWS Lambda
    use this parameter.

--- test/fire-web-hook-test.js
+++ test/fire-web-hook-test.js
@@ -11,6 +11,7 @@ describe("fire web hook", () => {
   before(() => {
     ck.freeze("2022-09-23");
     process.env.WEB_HOOK = "https://slack.url/hook/id";
+    process.env.WEB_HOOK_DEV = "https://slack.url/hook/dev";
     process.env.AUTH_TOKEN = "valid-token";
   });
   after(ck.defrost);
@@ -34,6 +35,31 @@ describe("fire web hook", () => {
       queryStringParameters: {
         type: "week",
         push: "valid-token",
+      },
+    });
+
+    expect(webHook.isDone()).to.equal(true);
+  });
+
+  it("sends request to development web hook", async () => {
+    const webHook = nock("https://slack.url", {
+      reqheaders: {
+
+        "content-type": "application/json",
+        "content-length": 75,
+      },
+    })
+      .post("/hook/dev", (body) => {
+        expect(body.text).to.equal("This shirt is ‘dry-clean only’ — which means it’s dirty.");
+        return true;
+      })
+      .reply(200);
+
+    await quote({
+      queryStringParameters: {
+        type: "week",
+        push: "valid-token",
+        dev: "true",
       },
     });
 
